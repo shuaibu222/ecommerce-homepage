d518705561fb93c1c5faefb67fbb87b0a0f81dec
shuaibu abdulkadir hussain <114912137+shuaibu222@users.noreply.github.com>
2023-01-31 10:13:06 +0100

    update
    update to the workflow and code

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# multi-step-form
- frontend mentor challenge
+## Overview
+
+- this is a frontend mentor challenge
+
+### Why this project?
+
+- I chose this project because it has some challenge-features that I have to train my self thinking programatically to bring well suited solution.
+
+### How do I get unstuck when am stuck
+
+- Usually I use freeCodeCamp's founder advice asking question, research and going through the code again and again till I succeed in finding best solution that worked
+
+### My process
+
+- Because this is a ready-made design from frontend mentor challenge I just follow thier instruction converting their design into a full website.
+
+### My challenges
+
+- Coding the modal in a responsive way tha will suite for both mobile and laptop/tablet is my main challenge on this project
+
+### Links
+
+- Live Site URL: [E-commerce-page](https://www.ecommercementor.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Javascript
+
+### What I learned
+
+- I learn advanced way of using flexbox in both the mobile and tablet approach.
+
+### Continued development
+
+- My area of concentration and focus next time is advanced Javascript.
+
+## Author
+
+- Website - [Shuaibu Abdulakdir](https://www.shuaibuabdulkadir.com)
--- script.js
+++ script.js
@@ -20,23 +20,58 @@ closeSidebar.addEventListener("click", function () {
   overlay.classList.add("fade-out");
 });
 
-// Image Slide DOM
+// Image Slide Code
 
-const bigImage = document.querySelectorAll(".b-image");
-const tabsSection = document.querySelector(".max-width-2");
-const tabsParent = document.querySelectorAll(".tabs-parent");
-console.log(tabsParent);
-// const tabs = document.querySelectorAll(".tabs");
+if (window.matchMedia("(min-width: 47em)").matches) {
+  // Image Slide DOM
 
-tabsSection.addEventListener("click", function (slider) {
-  const sliderId = slider.target.dataset.id;
-  console.log(sliderId);
+  const bigImage = document.querySelectorAll(".b-image");
+  const tabsSection = document.querySelector(".max-width-2");
+  const tabs = document.querySelectorAll(".tabs");
 
-  if (sliderId) {
-    tabsParent.forEach(function (t) {
-      t.classList.remove("active-bg");
-    });
+  tabsSection.addEventListener("click", function (slider) {
+    const sliderId = slider.target.dataset.id;
 
-    slider.target.classList.add("active-bg");
+    if (sliderId) {
+      tabs.forEach(function (t) {
+        t.classList.remove("active-bg");
+      });
+
+      slider.target.classList.add("active-bg");
+
+      bigImage.forEach(function (b) {
+        b.classList.remove("active");
+        b.classList.add("none");
+      });
+
+      const big = document.getElementById(sliderId);
+      big.classList.add("active");
+      big.classList.remove("none");
+    }
+  });
+} else {
+  // Mobile slider
+
+  const bigImage = document.querySelectorAll(".b-image");
+  let slideIndex = 1;
+  showDivs(slideIndex);
+
+  function plusDivs(n) {
+    showDivs((slideIndex += n));
   }
-});
+
+  function showDivs(n) {
+    let i;
+
+    if (n > bigImage.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = bigImage.length;
+    }
+    for (i = 0; i < bigImage.length; i++) {
+      bigImage[i].style.display = "none";
+    }
+    bigImage[slideIndex - 1].style.display = "block";
+  }
+}
